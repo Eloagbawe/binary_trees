9494bf322c143e8c119fb33338e9080e12b2cc98
Eloagbawe <eloagbawe@gmail.com>
2022-09-21 00:36:06 +0100

    fix errors

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,14 +9,9 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree != NULL)
 	{
-		if (tree->right != NULL)
-		{
-			binary_tree_delete(tree->right);
-		}
-		if (tree->left != NULL)
-		{
-			binary_tree_delete(tree->left);
-		}
+		binary_tree_delete(tree->right);
+		binary_tree_delete(tree->left);
+
 		tree = NULL;
 		free(tree);
 	}
