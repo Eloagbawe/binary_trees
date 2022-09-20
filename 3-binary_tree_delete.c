5639bca50e8bdf63dc9b39336bbc34176adcacc5
Eloagbawe <eloagbawe@gmail.com>
2022-09-21 00:44:17 +0100

    fix errors

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,19 @@
 #include "binary_trees.h"
 /**
+ * recursive_delete - deletes all nodes of a binary tree
+ * @tree: pointer to the root node of the tree to delete
+ * Return: void
+*/
+void recursive_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		recursive_delete(tree->right);
+		recursive_delete(tree->left);
+		free(tree);
+	}
+}
+/**
  * binary_tree_delete - deletes a binary tree
  * @tree: pointer to the root node of the tree to delete
  * Return: void
@@ -9,10 +23,7 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree != NULL)
 	{
-		binary_tree_delete(tree->right);
-		binary_tree_delete(tree->left);
-
+		recursive_delete(tree);
 		tree = NULL;
-		free(tree);
 	}
 }
